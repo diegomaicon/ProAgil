ade42f7c433b840fbf6efb5b1cab442e96b67eac
diego maicon silva <diego_maicon20@hotmail.com>
2020-10-06 23:11:27 -0300

    Listando  eventos

---
+++ ProAgil.WebAPI/Migrations/20201007011228_campo_imageURL_evento.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.WebAPI.Migrations
+{
+    public partial class campo_imageURL_evento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
